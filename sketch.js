621d9e4b55cc515e3b1aa901cbb6de6c57a2442f
Arsh-A-S <96184055+Arsh-A-S@users.noreply.github.com>
2022-01-01 18:16:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var name1,age1,grade1;
+function setup() {
+  createCanvas(400,400);
+  name1 = new Student("Arsh asif Shriwardankar",13,8 );
+  name1.show();
+}
+
+function draw() 
+{
+  background(30);
+}
