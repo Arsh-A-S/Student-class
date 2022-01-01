a2bc584a5819ef412e6ddf9e7c5908dba57d5833
Arsh-A-S <96184055+Arsh-A-S@users.noreply.github.com>
2022-01-01 18:18:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var name1,age1,grade1;
 function setup() {
   createCanvas(400,400);
-  name1 = new Student("Arsh asif Shriwardankar",13,8 );
+  name1 = new Student("Arsh asif Shriwardhankar",13,8 );
   name1.show();
 }
 
